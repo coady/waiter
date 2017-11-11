4d5d158268aacccc0db1553ee6f120575c2e83dd
Aric Coady <aric.coady@gmail.com>
2017-11-11 11:57:00 -0800

    Single predicate dispatch.

--- waiter/awaiter.py
+++ waiter/awaiter.py
@@ -1,14 +1,26 @@
 import asyncio
 import collections
+import functools
+import inspect
 import time
 from .base import suppress, waiter
+
+
+def overload(func):
+    base = getattr(waiter, func.__name__)
+    bind = inspect.signature(func).bind
+    (arg, predicate), = func.__annotations__.items()
+
+    def wrapper(*args, **kwargs):
+        match = predicate(bind(*args, **kwargs).arguments[arg])
+        return (func if match else base)(*args, **kwargs)
+    return functools.update_wrapper(wrapper, base)
 
 
 class awaiter(waiter):
     __doc__ = waiter.__doc__
 
     async def __aiter__(self):
-        """Asynchronously generate a slow loop of elapsed time."""
         start = time.time()
         yield 0.0
         for delay in self.delays:
@@ -17,46 +29,30 @@ class awaiter(waiter):
                 break
             await asyncio.sleep(min(delay, remaining))
             yield time.time() - start
+    __aiter__.__doc__ = waiter.__iter__.__doc__
 
-    async def athrottle(self, aiterable):
-        anext = aiterable.__aiter__().__anext__
+    @overload
+    async def throttle(self, iterable: lambda it: isinstance(it, collections.AsyncIterable)):
+        anext = iterable.__aiter__().__anext__
         with suppress(StopAsyncIteration):
             async for _ in self:  # noqa
                 yield await anext()
 
-    async def arepeat(self, func, *args, **kwargs):
+    @overload
+    async def repeat(self, func: asyncio.iscoroutinefunction, *args, **kwargs):
         async for _ in self:  # noqa
             yield await func(*args, **kwargs)
 
-    async def aretry(self, exception, func, *args, **kwargs):
+    @overload
+    async def retry(self, exception, func: asyncio.iscoroutinefunction, *args, **kwargs):
         async for _ in self:  # noqa
             with suppress(exception) as excs:
                 return await func(*args, **kwargs)
         raise excs[0]
 
-    async def apoll(self, predicate, func, *args, **kwargs):
-        async for result in self.arepeat(func, *args, **kwargs):
+    @overload
+    async def poll(self, predicate, func: asyncio.iscoroutinefunction, *args, **kwargs):
+        async for result in self.repeat(func, *args, **kwargs):
             if predicate(result):
                 return result
         raise StopAsyncIteration
-
-    def throttle(self, iterable):
-        """Delay iteration, or async iteration."""
-        if isinstance(iterable, collections.AsyncIterable):
-            return self.athrottle(iterable)
-        return super().throttle(iterable)
-
-    def repeat(self, func, *args, **kwargs):
-        """Repeat function or coroutine call."""
-        method = self.arepeat if asyncio.iscoroutinefunction(func) else super().repeat
-        return method(func, *args, **kwargs)
-
-    def retry(self, exception, func, *args, **kwargs):
-        """Repeat function or coroutine call until exception isn't raised."""
-        method = self.aretry if asyncio.iscoroutinefunction(func) else super().retry
-        return method(exception, func, *args, **kwargs)
-
-    def poll(self, predicate, func, *args, **kwargs):
-        """Repeat function or coroutine call until predicate evaluates to true."""
-        method = self.apoll if asyncio.iscoroutinefunction(func) else super().poll
-        return method(predicate, func, *args, **kwargs)
